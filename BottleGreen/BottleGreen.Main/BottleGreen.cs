fc72195926b90b84627b04a6bf32dee650a8eeb5
Denis Neychev <ufreezybg@gmail.com>
2015-07-24 00:54:44 +0300

    Problem 2 done + basic UI.

---
+++ BottleGreen/BottleGreen.Main/BottleGreen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _02_ZipRepportsToMSSQL;
+
+namespace BottleGreen.Main
+{
+    public partial class BottleGreen : Form
+    {
+        public BottleGreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var d = new DataMigrator();
+            d.MigrateDataFromExcelFiles();
+            MessageBox.Show("Operation completed! It took us " + sw.Elapsed.TotalSeconds +" seconds to process your request!");
+        }
+
+        private void BottleGreen_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
--- BottleGreen/BottleGreen.Main/Program.cs
+++ BottleGreen/BottleGreen.Main/Program.cs
@@ -16,7 +16,7 @@ namespace BottleGreen.Main
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new BottleGreen());
         }
     }
 }
