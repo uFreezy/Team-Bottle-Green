8cba1d67cf0a8d3b30f1fc1b1124cc70ac971c74
Milen <milendimitrow@abv.bg>
2015-07-26 23:19:28 +0300

    task 4 almost done :D

--- BottleGreen/BottleGreen.Main/BottleGreen.Designer.cs
+++ BottleGreen/BottleGreen.Main/BottleGreen.Designer.cs
@@ -33,10 +33,15 @@
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.endDateLabel = new System.Windows.Forms.Label();
+            this.endDate = new System.Windows.Forms.TextBox();
+            this.startDateLabel = new System.Windows.Forms.Label();
+            this.startDate = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.PDF_Reports = new System.Windows.Forms.Button();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.exit = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -45,7 +50,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(6, 54);
+            this.button1.Location = new System.Drawing.Point(6, 131);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 40);
             this.button1.TabIndex = 0;
@@ -58,7 +63,7 @@
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(100, 100);
+            this.groupBox1.Size = new System.Drawing.Size(100, 177);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Problem 2";
@@ -66,9 +71,9 @@
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Location = new System.Drawing.Point(12, 118);
+            this.groupBox2.Location = new System.Drawing.Point(436, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(100, 100);
+            this.groupBox2.Size = new System.Drawing.Size(100, 177);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Problem 6";
@@ -76,7 +81,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(6, 54);
+            this.button2.Location = new System.Drawing.Point(6, 131);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(88, 40);
             this.button2.TabIndex = 0;
@@ -86,17 +91,58 @@
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.endDateLabel);
+            this.groupBox3.Controls.Add(this.endDate);
+            this.groupBox3.Controls.Add(this.startDateLabel);
+            this.groupBox3.Controls.Add(this.startDate);
             this.groupBox3.Controls.Add(this.button3);
             this.groupBox3.Location = new System.Drawing.Point(224, 12);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(100, 100);
+            this.groupBox3.Size = new System.Drawing.Size(100, 177);
             this.groupBox3.TabIndex = 3;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Problem 4";
             // 
+            // endDateLabel
+            // 
+            this.endDateLabel.AutoSize = true;
+            this.endDateLabel.Location = new System.Drawing.Point(6, 78);
+            this.endDateLabel.Name = "endDateLabel";
+            this.endDateLabel.Size = new System.Drawing.Size(52, 13);
+            this.endDateLabel.TabIndex = 4;
+            this.endDateLabel.Text = "end date:";
+            this.endDateLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // endDate
+            // 
+            this.endDate.Location = new System.Drawing.Point(6, 94);
+            this.endDate.Multiline = true;
+            this.endDate.Name = "endDate";
+            this.endDate.Size = new System.Drawing.Size(88, 20);
+            this.endDate.TabIndex = 3;
+            this.endDate.TabStop = false;
+            this.endDate.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // startDateLabel
+            // 
+            this.startDateLabel.AutoSize = true;
+            this.startDateLabel.Location = new System.Drawing.Point(6, 35);
+            this.startDateLabel.Name = "startDateLabel";
+            this.startDateLabel.Size = new System.Drawing.Size(54, 13);
+            this.startDateLabel.TabIndex = 2;
+            this.startDateLabel.Text = "start date:";
+            // 
+            // startDate
+            // 
+            this.startDate.Location = new System.Drawing.Point(6, 51);
+            this.startDate.Multiline = true;
+            this.startDate.Name = "startDate";
+            this.startDate.Size = new System.Drawing.Size(88, 20);
+            this.startDate.TabIndex = 1;
+            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(6, 54);
+            this.button3.Location = new System.Drawing.Point(6, 131);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(88, 40);
             this.button3.TabIndex = 0;
@@ -109,14 +155,14 @@
             this.groupBox4.Controls.Add(this.PDF_Reports);
             this.groupBox4.Location = new System.Drawing.Point(118, 12);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(100, 100);
+            this.groupBox4.Size = new System.Drawing.Size(100, 177);
             this.groupBox4.TabIndex = 4;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Problem 3";
             // 
             // PDF_Reports
             // 
-            this.PDF_Reports.Location = new System.Drawing.Point(6, 54);
+            this.PDF_Reports.Location = new System.Drawing.Point(6, 131);
             this.PDF_Reports.Name = "PDF_Reports";
             this.PDF_Reports.Size = new System.Drawing.Size(88, 40);
             this.PDF_Reports.TabIndex = 0;
@@ -128,16 +174,27 @@
             // 
             this.groupBox5.Location = new System.Drawing.Point(330, 12);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(100, 100);
+            this.groupBox5.Size = new System.Drawing.Size(100, 177);
             this.groupBox5.TabIndex = 5;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Problem 5";
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(460, 295);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(70, 38);
+            this.exit.TabIndex = 6;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
             // 
             // BottleGreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 231);
+            this.ClientSize = new System.Drawing.Size(544, 345);
+            this.Controls.Add(this.exit);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
@@ -149,6 +206,7 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -165,6 +223,11 @@
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.Button PDF_Reports;
         private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label endDateLabel;
+        private System.Windows.Forms.TextBox endDate;
+        private System.Windows.Forms.Label startDateLabel;
+        private System.Windows.Forms.TextBox startDate;
+        private System.Windows.Forms.Button exit;
     }
 }
 
--- BottleGreen/BottleGreen.Main/BottleGreen.cs
+++ BottleGreen/BottleGreen.Main/BottleGreen.cs
@@ -9,8 +9,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
+using _04_VendorReportToXML;
 using _02_ZipRepportsToMSSQL;
 using _06_XMLToMSSQL;
+using System.Threading;
+using System.Globalization;
 
 
 namespace BottleGreen.Main
@@ -61,11 +64,46 @@ namespace BottleGreen.Main
         private void MsqlToXML_Click(object sender, EventArgs e)
         {
 
+           Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+           try
+           {
+               var startDate = DateTime.Parse(this.startDate.Text);
+               var endDate = DateTime.Parse(this.startDate.Text);
+               XmlHelper xmlFileManager = new XmlHelper();
+               var salesReport = xmlFileManager.ReadData(startDate, endDate);
+               string XmlResultFileName = @"..\SalesByVendorReport.xml";
+               xmlFileManager.ExportToXml(salesReport, XmlResultFileName);
+               this.startDate.Text = "";
+               this.endDate.Text = "";
+               MessageBox.Show(@"MS SQL Export to XML file has been finished!");
+           }
+           catch (System.FormatException ex)
+           {
+               MessageBox.Show(
+                   ex.Message + Environment.NewLine
+                   + @"Date must be in format: ""YYYY.DD.MM""");
+           }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             Process.Start("Task3.exe");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
