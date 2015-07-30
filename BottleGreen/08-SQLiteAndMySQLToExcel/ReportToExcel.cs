34220a67965e4cb9a17f1e659249cf0b5798e51e
asyamincheva <asya.m.mincheva@gmail.com>
2015-07-30 20:39:09 +0300

    problem 8 - class sales

---
+++ BottleGreen/08-SQLiteAndMySQLToExcel/ReportToExcel.cs
@@ -0,0 +1,19 @@
+﻿namespace _08_SQLiteAndMySQLToExcel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Data.SQLite;
+    using System.Globalization;
+    using System.IO;
+    using System.Reflection;
+    using MySql.Data.MySqlClient;
+    using SQL = System.Data;
+    using Excel = Microsoft.Office.Interop.Excel;
+
+    public class ReportToExcel
+    {
+    }
+}
